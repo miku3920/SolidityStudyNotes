ae98d7d68967d85c4dc5e167cb791c9e39d221e5
miku3920 <papa964204@gmail.com>
2024-11-17 07:03:41 +0000

    truffle dashboard

--- truffle-config.js
+++ truffle-config.js
@@ -47,6 +47,11 @@
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
+  dashboard: {
+    host: '0.0.0.0',
+    port: 24012,
+  },
+
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -65,7 +70,7 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
+      host: "0.0.0.0",       // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
