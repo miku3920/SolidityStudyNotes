f271ffbe9e964660cb10d5c84371d449b91e5bc3
miku3920 <papa964204@gmail.com>
2024-11-24 13:22:06 +0800

    drawAndPayMoneyToWinner & onlyManager

--- contracts/GMR.sol
+++ contracts/GMR.sol
@@ -3,14 +3,34 @@ pragma solidity ^0.7.4;
 
 contract GMR {
     address public manager;
-    address[] public players;
+    address payable[] public players;
 
     constructor() {
         manager = msg.sender;
     }
 
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
     function enterGame() public payable {
         require(msg.value > 0.01 ether);
         players.push(msg.sender);
+    }
+
+    function chooseByTime() private view returns (uint256) {
+        return block.timestamp % players.length;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function drawAndPayMoneyToWinner() public onlyManager {
+        uint256 index = chooseByTime();
+        players[index].transfer(getBalance());
+
+        players = new address payable[](0);
     }
 }
