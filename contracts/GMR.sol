9e83bc8f095ec57e8cf595304f8d0e6aee667c15
miku3920 <papa964204@gmail.com>
2024-11-24 03:18:32 +0000

    GMR enterGame

--- contracts/GMR.sol
+++ contracts/GMR.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.7.4;
 
 contract GMR {
     address public manager;
+    address[] public players;
 
     constructor() {
         manager = msg.sender;
+    }
+
+    function enterGame() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
     }
 }
